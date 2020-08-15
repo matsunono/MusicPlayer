19f9304adaaf5e6a2eedac42a9494b70a7e573f5
matsunono <swing.hokke@gmail.com>
2020-08-15 23:35:27 +0900

    カメラ画像のキャプチャまで出来た

--- MusicPlayer/Form1.Designer.cs
+++ MusicPlayer/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.StartButton.Font = new System.Drawing.Font("UD デジタル 教科書体 NK-B", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.StartButton.Location = new System.Drawing.Point(170, 353);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(114, 68);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Caputure";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(170, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- MusicPlayer/Form1.cs
+++ MusicPlayer/Form1.cs
@@ -7,14 +7,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenCvSharp;
 
 namespace MusicPlayer
 {
     public partial class Form1 : Form
     {
+        int WIDTH = 640;
+        int HEIGHT = 480;
+        Mat frame;
+        VideoCapture capture;
+        Bitmap bitmap;
+        Graphics graphics;
+
         public Form1()
         {
             InitializeComponent();
+
+            // カメラ画像取得用のVideoCapture作成
+            capture = new VideoCapture(0);
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("camera was not found!");
+                this.Close();
+            }
+            capture.FrameWidth = WIDTH;
+            capture.FrameHeight = HEIGHT;
+
+            // 取得先のMat作成(CV_8UC3 : 3個のCV_8U(符号なし8bit整数)カラー画像の初期値)
+            frame = new Mat(HEIGHT, WIDTH, MatType.CV_8UC3);
+
+            // 表示用のBitmap作成
+            // (Format24bppRgb : 1ピクセルあたり24bitの形式であることを指定
+            // つまり、赤、緑、および青のコンポーネントに、それぞれ8bitを使用)
+            bitmap = new Bitmap(frame.Cols, frame.Rows, (int)frame.Step(),
+                System.Drawing.Imaging.PixelFormat.Format24bppRgb, frame.Data);
+
+            // PictureBoxを出力サイズに合わせる
+            pictureBox1.Width = frame.Cols / 2;
+            pictureBox1.Height = frame.Rows / 2;
+
+            // 描画用のGraphics作成
+            graphics = pictureBox1.CreateGraphics();
+
+            // 画像取得スレッド開始
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            frame.SaveImage(@"\img\cap.png");
+            using(Mat cap = new Mat(@"\img\cap.png"))
+            {
+                // 保存された画像の出力
+                Cv2.ImShow("test1", frame);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+            while (backgroundWorker1.IsBusy)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bw = (BackgroundWorker)sender;
+
+            while (!backgroundWorker1.CancellationPending)
+            {
+                // 画像取得
+                capture.Grab();
+                NativeMethods.videoio_VideoCapture_operatorRightShift_Mat(capture.CvPtr, frame.CvPtr);
+                bw.ReportProgress(0);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // bitmapを左上からフレームの端まで描画
+            graphics.DrawImage(bitmap, 0, 0, frame.Cols/2, frame.Rows/2);
         }
     }
 }
